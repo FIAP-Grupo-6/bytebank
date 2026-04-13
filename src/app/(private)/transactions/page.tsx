ffb8f7b47e8cae4b490573749fc209e10cdabbc9
Edson Oliveira <edson.byteon@hotmail.com>
2026-04-13 20:53:20 -0300

    Remover importação não utilizada do ícone Trash2

--- src/app/(private)/transactions/page.tsx
+++ src/app/(private)/transactions/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Plus, Trash2 } from "lucide-react";
+import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import Title from "@/components/shared/title";
 import ListItem from "@/components/shared/list-item";

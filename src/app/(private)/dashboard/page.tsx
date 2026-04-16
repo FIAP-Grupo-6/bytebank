a37965da325035b1f2a8f1af130c5cdd8f748c87
José Lázaro <contato.jlazaro@gmail.com>
2026-04-15 21:47:56 -0300

    feat: add elements in layout

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -4,5 +4,9 @@ import { useState } from 'react';
 export default function Dashboard() {
   const [data, setData] = useState<any>(null);
 
-  return <div>Dashboard</div>;
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+    </div>
+  );
 }

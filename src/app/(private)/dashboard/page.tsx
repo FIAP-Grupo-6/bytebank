99e7fb7a88dceb602bcf547b2276f998f1e213df
Luiz Otavio <locchavescarvalho@gmail.com>
2026-04-17 18:18:33 -0300

    refactor(dashboard): integrate dashboard page with hook and updated components

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,10 +1,20 @@
+'use client'
+
 import { Button } from '@/components/shared/button';
 import { BalanceCard } from '@/features/dashboard/balance-card';
+import { DashboardSkeleton } from '@/features/dashboard/dashboard-skeleton';
 import { Extract } from '@/features/dashboard/extract';
 import { SummaryCard } from '@/features/dashboard/summary-card';
+import { useDashboard } from '@/hooks/dashboard/use-dashboard';
 import { Plus } from 'lucide-react';
 
 export default function Dashboard() {
+  const { balance, income, expense, recentTransactions, loading } = useDashboard()
+
+  if(loading) {
+    return <DashboardSkeleton />
+  }
+
   return (
     <div className="flex flex-col gap-8">
       <div className="flex items-center justify-between">
@@ -16,14 +26,12 @@ export default function Dashboard() {
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-        <BalanceCard />
-        <SummaryCard />
+        <BalanceCard value={balance} />
+        <SummaryCard income={income} expense={expense} />
       </div>
-     
 
-      <Extract />
+      <Extract transactions={recentTransactions} />
       
-
       <Button
         className="fixed bottom-6 right-6 md:hidden shadow-lg"
         icon={Plus}

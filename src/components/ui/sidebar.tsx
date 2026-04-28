37612029b19c7259031af74d14842863ad0c8988
José Lázaro <contato.jlazaro@gmail.com>
2026-04-28 20:20:07 -0300

    fix: style

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -270,8 +270,8 @@ const SidebarTrigger = React.forwardRef<
       ref={ref}
       data-sidebar="trigger"
       variant="ghost"
-      size="icon"
-      className={cn('h-7 w-7', className)}
+      size="sm"
+      className={cn('h-7 w-7 p-0', className)}
       onClick={(event) => {
         onClick?.(event);
         toggleSidebar();
